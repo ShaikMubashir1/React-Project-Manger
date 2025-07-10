904317f9fe937dc96277bdb185ebf4c5dfe6596e
Mubashir <shaikmubashir7411@gmail.com>
2025-07-10 11:07:11 +0530

    Adding Image

--- src/component/NewProject.jsx
+++ src/component/NewProject.jsx
@@ -2,7 +2,7 @@ function NewProject({ onAdding }) {
   return (
     <div className=" flex justify-center w-full mt-10 md:py-10">
       <div className=" flex flex-col justify-start items-center gap-5 ">
-        <img src="/no-projects.png" alt="Image" className=" w-20 md:w-40 " />
+        <img src="./no-projects.png" alt="Image" className=" w-20 md:w-40 " />
         <h2 className=" text-stone-600 font-bold text-2xl">
           No Project Selected
         </h2>
